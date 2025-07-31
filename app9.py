34159c1e454b86fc5d99f9fe76ff22762f2cabd4
naseema-shaik19 <sknaseema246@gmail.com>
2025-07-31 09:25:45 +0530

    adding new py file

--- app9.py
+++
@@ -1,9 +0,0 @@
-number=10
-scores=[10,20,30]
-names=["alexa","siri","jarvis"]
-for i in range(number):
-    print(i)
-for score in scores:
-    print(score)
-for name in names:
-    print(name)       
